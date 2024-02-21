9bfb22f43d120dc65deb2a2d52dc09709384b608
Daniel Chinga <danielchingasilva@gmail.com>
2024-02-21 07:29:41 -0300

    Ejemplo_SQL_con_Python.py V1.2

--- Ejemplo_SQL_con_Python.py
+++ Ejemplo_SQL_con_Python.py
@@ -42,6 +42,7 @@ conn.close()
 #Para hacer funciones en python que se utilicen en la consulta
 
 #import sqlite3
+#import pandas as pd
 
 #definimos una funcion
 #K(x) = x^2
@@ -51,5 +52,45 @@ Square = Lambda n: n*n
 
 conn = sqlite3.connect("Northwind.db")
 
+#Crea una funcion para utilizar en la base de datos
+#square de la izquierda es el nombre de la funcion, el de la derecha es la funcion entregada
+conn.create_function("square",1, square)
 
+#Crea una consulta y la almacena en cursor, se deben cerrar
+cursor = conn.cursor()
+cursor.execute("""
+               SELECT *, square(Price) FROM Products""")
+
+#El resulado de la consulta de obtiene por fetchall() almacenado en results
+#pero se entrega en forma de lista con tuplas, no se muestra como tabla
+results = cursor.fetchall()
+
+#Los ordena muy similar para visual como tablas de SQL
+results_df = pd.DataFrame(results)
+
+#hace efectivo cualquier tipo de cambio
+conn.commit()
+
+#cierra la coneccion con el cursor
+cursor.close()
+
+conn.close()
+
+
+#------------------------------------------------------------------------------------------
+#Se puede crear un contexto para evitar crear conecciones
+
+#import sqlite3
+#importa pandas as pd
+
+with sqlite3.connect("Northwind.db") as conn:
+    conn.create_function("square", 1, square)
+    cursor = conn.cursor()
+    cursor.execute("SELECT *, square(Price) FROM Products")
+    results = cursor.fetchall()
+    results_df = pd.DataFrame(results)
+
+print(results_df)
+
+#aca no se cierran automaticamente las conecciones
 
